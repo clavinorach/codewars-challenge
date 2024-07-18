a8ccd3693dc5f8424002f833a761fc32e2bdc505
clavinorach <clavinorach@gmail.com>
2024-07-19 00:27:41 +0700

    new practice

--- 6-dna-to-rna-CW.js
+++ 6-dna-to-rna-CW.js
@@ -1,16 +1,29 @@
-function DNAtoRNA(dna) {
-    let rna = '';
-    for(let i = 0; i < dna.length; i++){
-    //     if(dna[i] === 'T'){
-    //         rna += 'U';
-    //     } else {
-    //         rna += dna[i];
-    //     }
-    //     rna =+ dna[i];
-    // }
-    rna += (dna[i] === 'T') ? 'U' : dna[i];
-    }
-    return dna;
-}
+// function DNAtoRNA(dna) {
+//     let rna = '';
+//     for(let i = 0; i < dna.length; i++){
+//     //     if(dna[i] === 'T'){
+//     //         rna += 'U';
+//     //     } else {
+//     //         rna += dna[i];
+//     //     }
+//     //     rna =+ dna[i];
+//     // }
+//     rna += dna[i] === 'T' ? 'U' : dna[i];
+//     }
+//     return dna;
+// }
 
-console.log(DNAtoRNA("GCAT"));
+// function DNAtoRNA(dna) {
+//     return dna
+//     .split('')
+//     .map((el) => (el === 'T' ? 'U' :  el))
+//     .join('')
+// }
+
+//High order function
+// const DNAtoRNA = (dna) => dna.split('').map((el) => (el === 'T' ? 'U' :el)).join('');
+
+//regex
+const DNAtoRNA = (dna) => dna.replaceAll(/T/g, 'U');
+
+console.log(DNAtoRNA("TTTT"));
