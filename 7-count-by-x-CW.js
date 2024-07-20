d26572a732b069d7a07ac610c2d931afcc02c7b6
clavinorach <clavinorach@gmail.com>
2024-07-20 14:44:13 +0700

    new practice

---
+++ 7-count-by-x-CW.js
@@ -0,0 +1,13 @@
+// function countBy(x, n) {
+//     let z = [];
+//     for(let i = 1; i<=n; i++) {
+//         z.push(i * x);
+//     }
+
+//     return z;
+// }
+
+
+const countBy = (x, n) => [...Array(n)].map((el, i) => (i + 1) * x);
+
+console.log(countBy(2, 5));
